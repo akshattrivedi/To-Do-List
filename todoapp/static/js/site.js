b248fa28fe331eb752548954c3e3d3054dc69313
akshattrivedi <akshattrivedi@github.com>
2023-04-11 17:53:36 +0530

    conf: vercel database open file

--- todoapp/static/js/site.js
+++ todoapp/static/js/site.js
@@ -74,3 +74,14 @@ $(document).ready(function() {
   init();
 
 });
+
+import { readFileSync } from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const file = path.join(process.cwd(), 'files', 'test.json');
+  const stringified = readFileSync(file, 'utf8');
+
+  res.setHeader('Content-Type', 'application/json');
+  return res.end(stringified);
+}
